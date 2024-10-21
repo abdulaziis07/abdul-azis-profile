9a0ea847be23150a97d647d6a0a395ddb995da05
abdulaziis07 <azis190407@gmail.com>
2024-10-21 22:46:38 +0700

    fixed: image component width

--- src/components/elements/Image.tsx
+++ src/components/elements/Image.tsx
@@ -9,7 +9,7 @@ type ImageComponentProps = {
 export const Image = ({ src, alt, className }: ImageComponentProps) => {
   return (
     <AspectRatio ratio={1 / 1}>
-      <img aria-label="image" src={src} alt={alt} className={`w-48 md:w-64 lg:w-96 opacity-90 ml-4 rounded-full shadow-xl drop-shadow-xl ring-1 ring-slate-900 animate-spin-slow transition-all ease-in-out hover:opacity-100 ${className}`} />
+      <img aria-label="image" src={src} alt={alt} className={`w-48 lg:w-64 opacity-90 rounded-full ml-3 shadow-xl drop-shadow-xl ring-1 ring-slate-900 animate-spin-slow transition-all ease-in-out hover:opacity-100 ${className}`} />
     </AspectRatio>
   )
 }
