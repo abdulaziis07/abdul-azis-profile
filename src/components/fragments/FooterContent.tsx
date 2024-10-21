6613851c149bb05dd393d4843294eaa8577b713e
abdulaziis07 <azis190407@gmail.com>
2024-10-21 23:06:10 +0700

    feat: add new social icons and change text in footer components

--- src/components/fragments/FooterContent.tsx
+++ src/components/fragments/FooterContent.tsx
@@ -3,15 +3,16 @@ import { Description } from "@/components/elements/Description";
 import {
   GitHubLogoIcon,
   InstagramLogoIcon,
-  TwitterLogoIcon
+  TwitterLogoIcon,
+  DiscordLogoIcon
 } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 import { socialLinks } from "@/utils/socialLinks";
 
 export const FooterContent = ({ author }: { author: string }) => {
   return (
-    <div className="flex flex-col items-center">
-      <div className="flex items-center gap-2 mb-2">
+    <div className="flex items-center space-x-2">
+      <div className="grid grid-cols-2 -p-2">
         <div>
           <a href={socialLinks[0].href} target="_blank">
             <Button variant={"ghost"}>
@@ -33,12 +34,19 @@ export const FooterContent = ({ author }: { author: string }) => {
             </Button>
           </a>
         </div>
+        <div>
+          <a href="">
+            <Button variant={"ghost"}>
+              <DiscordLogoIcon />
+            </Button>
+          </a>
+        </div>
       </div>
       <div>
         <Description>
           &copy; {copyrightTime.toString()} by {author}
         </Description>
-        <Description>All right them reserved.</Description>
+        <Description className="ml-4">All rights reserved.</Description>
       </div>
     </div>
   );
